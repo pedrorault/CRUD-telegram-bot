9c56758269a0c38e3388c382e1d4266c91a0e480
Pedro R Costa <pedrorault@gmail.com>
2020-11-28 21:07:01 -0300

    web server

---
+++ web.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+PORT = os.getenv("PORT")
+
+class Redirect(BaseHTTPRequestHandler):
+   def do_GET(self):
+       self.send_response(302)
+       self.send_header('Location', "http://t.me/BD2020EACH_Bot")
+       self.end_headers()
+
+HTTPServer(("", int(PORT)), Redirect).serve_forever()
